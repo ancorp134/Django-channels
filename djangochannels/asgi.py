2fd1c9d0eaeff56f94f6a167975141afc28391bb
Ankit Tyagi <ancorp007@gmail.com>
2023-01-19 11:16:24 +0530

    testing server

--- djangochannels/asgi.py
+++ djangochannels/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from app.consumers import MyConsumer
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochannels.settings')
 
 application = get_asgi_application()
+
+ws_patterns = [
+    path('ws/test/',MyConsumer),
+]
+
+application = ProtocolTypeRouter({
+    'websocket' : URLRouter(ws_patterns)
+})
