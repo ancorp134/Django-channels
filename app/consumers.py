2dc052180d51cd8cada47eed1b679a5f6bcd129a
Ankit Tyagi <ancorp007@gmail.com>
2023-01-19 12:25:29 +0530

    testing server

--- app/consumers.py
+++ app/consumers.py
@@ -1,25 +1,39 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.consumer import SyncConsumer
 
-class MyConsumer(WebsocketConsumer):
+from channels.consumer import AsyncConsumer
+
+class EchoConsumer(AsyncConsumer):
+
+    async def websocket_connect(self, event):
+        await self.send({
+            "type": "websocket.accept",
+        })
+
+    async def websocket_receive(self, event):
+        await self.send({
+            "type": "websocket.send",
+            "text": event["text"],
+        })
+# from channels.generic.websocket import WebsocketConsumer
+# from asgiref.sync import async_to_sync
+# import json
+
+# class MyConsumer(WebsocketConsumer):
 
 
-    def connect(self):
+#     def connect(self):
+#         self.room_name = "test_consumer"
+#         self.room_group_name = "test_consumer_group"
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_name , self.room_group_name
+#         )
+#         self.accept()
+#         self.send(text_data= json.dumps({ 'status' : 'connected' }))
         
-        self.accept()
-      
-        self.accept("subprotocol")
         
-        self.close()
 
-    def receive(self, text_data=None, bytes_data=None):
-        
-        self.send(text_data="Hello world!")
-        
-        self.send(bytes_data="Hello world!")
-       
-        self.close()
-        
-        self.close(code=4123)
+#     def receive(self, text_data=None, bytes_data=None):
+#         pass
 
-    def disconnect(self, close_code):
-        pass
+#     def disconnect(self, close_code):
+#         pass
